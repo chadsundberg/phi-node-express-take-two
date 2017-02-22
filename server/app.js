19721998e4aeee82f3cfeed068433dd8271fe96d
Chad Sundberg <chadsundberg@gmail.com>
2017-02-22 15:22:35 -0600

    working

--- server/app.js
+++ server/app.js
@@ -1,1 +1,51 @@
 console.log('starting up the server');
+
+var express = require('express');  // this path defaults to the node modules folder and finds it
+var app = express();
+
+app.use(express.static('server/public')); // checks to see if this request is a request for a static file
+// server/public is where our static files live
+
+var fishiesList = [{name:'walleye'}, {name: 'pike'}, {name: 'muskie'}];
+
+app.get('/fish', function(req, res) {  // node has already created this req and res object
+    // res.send is necessary to always give a response from the server.
+  res.send(fishiesList);
+});
+
+app.get('/fish/first', function(req, res) {  // node has already created this req and res object
+    // res.send is necessary to always give a response from the server.
+  res.send(fishiesList[0]);
+});
+
+// handle the request for the first fish
+app.get('/fish/first', function(req, res){
+  res.send(fishiesList[0]);
+});
+
+// create a new route for last fishiesList
+
+app.get('/fish/last', function(req, res){
+  // var lastIndex = fishiesList.length - 1;
+  // res.send(fishiesList[lastIndex]);
+  res.send(fishiesList[fishiesList.length - 1]);
+});
+
+//handle request for the name of the last fish
+
+app.get('/fish/last/name', function(req, res) {
+  var lastIndex = fishiesList.length - 1;
+  res.send(fishiesList[lastIndex].name);
+
+});
+
+app.get('/fish/first/name', function(req, res) {
+  // var lastIndex = fishiesList;
+  res.send(fishiesList[0].name);
+
+});
+
+
+
+
+app.listen(5000);
--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,25 @@
 console.log('sourced!');
+$(document).ready(function() {
+  $.ajax({
+    type:'GET',
+    url: '/fish',
+    success: function(response) {
+      console.log('response', response);
+      // we are about to loop through our fishies array and add it to the document
+
+for (var i = 0; i < response.length; i++) {
+  $('#fishTank').append('<li>' + response[i].name + '</li>');
+}
+}
+});
+
+
+$.ajax({
+  type:'GET',
+  url: '/fish/first/name',
+  success: function(response) {
+    console.log('response', response);
+    $('#firstFishy').text(response);
+  }
+});
+});
